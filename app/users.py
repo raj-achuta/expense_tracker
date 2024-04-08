a0bd936f9aab829e0a0e780b689c8c561668854c
Raj🤖 <raj.achut@outlook.com>
2024-04-08 13:24:16 +0530

    update logger

--- app/users.py
+++ app/users.py
@@ -5,6 +5,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 class Users:
     def __init__(self, allowed_users: str) -> None:
@@ -15,27 +16,27 @@ class Users:
     def _add_user(self, userid: str) -> None:
         if userid not in self.users:
             self.users[userid] = []
-            logging.info(f"User '{userid}' added successfully.")
+            logger.info(f"User '{userid}' added successfully.")
         else:
-            logging.info(f"User '{userid}' already exists.")
+            logger.info(f"User '{userid}' already exists.")
             
     def is_user_exists(self, userid: str) -> bool:
         return self.users.__contains__(userid)
     
     def add_data(self, userid: str, data: str):
         if(not self.is_user_exists(userid)):
-            logging.info(f"User '{userid}' not present in the system to add data.")
+            logger.info(f"User '{userid}' not present in the system to add data.")
             return
         self.users.get(userid).append(data)
         
     def get_data(self, userid: str):
         if(not self.is_user_exists(userid)):
-            logging.info(f"User '{userid}' not present in the system to add data.")
+            logger.info(f"User '{userid}' not present in the system to add data.")
             return
         return self.users.get(userid)
     
     def clear_data(self, userid: str):
         if(not self.is_user_exists(userid)):
-            logging.info(f"User '{userid}' not present in the system to clear data.")
+            logger.info(f"User '{userid}' not present in the system to clear data.")
             return
         self.users.get(userid).clear()
